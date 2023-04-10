e60a063a2eb9048fc5a9daee5ff04e802fbb4674
jenkins-push <non_111111@hotmail.com>
2023-04-10 15:16:12 +1000

    update data actionlistener

--- src/main/java/com/tutorialspoint/test/HelloWorld.java
+++ src/main/java/com/tutorialspoint/test/HelloWorld.java
@@ -1,15 +1,30 @@
 package com.tutorialspoint.test;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.event.ActionEvent;
 
 @ManagedBean(name = "helloWorld", eager = true)
 public class HelloWorld {
    
+	private String message = "hello hello";
+	
    public HelloWorld() {
       System.out.println("HelloWorld started!");
    }
 	
    public String getMessage() {
-      return "Hello World!";
+      return message;
+   }
+   
+   public void setMessage(String message) {
+       this.message = message;
+   }
+   
+   public void updateData(String input1) {
+	   message = "updated message: " + input1;
+   }
+   
+   public String showResult() {
+	      return "snow result";
    }
 }
