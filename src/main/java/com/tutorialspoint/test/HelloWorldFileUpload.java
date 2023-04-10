e0fb944e89ec39fda7a435c9d161300a0f677524
jenkins-push <non_111111@hotmail.com>
2023-04-10 21:33:40 +1000

    hello world fileupload

--- src/main/java/com/tutorialspoint/test/HelloWorldFileUpload.java
+++ src/main/java/com/tutorialspoint/test/HelloWorldFileUpload.java
@@ -1,20 +1,36 @@
 package com.tutorialspoint.test;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.event.ActionEvent;
+import javax.servlet.ServletException;
+import javax.servlet.http.Part;
 
-@ManagedBean(name = "helloWorld", eager = true)
-public class HelloWorld {
+@ManagedBean(name = "helloWorldFileUpload", eager = true)
+public class HelloWorldFileUpload {
    
 	private String message = "hello hello";
+    private Part file;
+    
+    public Part getFile() {
+        return file;
+    }
+ 
+    public void setFile(Part file) {
+        this.file = file;
+    }
 	
-   public HelloWorld() {
-      System.out.println("HelloWorld started!");
+   public HelloWorldFileUpload() {
+      System.out.println("HelloWorld file uplaod started!");
    }
 	
    public String getMessage() {
@@ -25,15 +41,29 @@ public class HelloWorld {
        this.message = message;
    }
    
-   public void updateData(String input1) {
+   public void upload() {
    
+	   String text=null;
+	   try{
+       		Part filepart = getFile();
+       		InputStream fileContent = filepart.getInputStream();
+       		text = new String(fileContent.readAllBytes(), StandardCharsets.UTF_8);
+       } catch(FileNotFoundException e) {
+       	message = "filenotfoundexception " + e;        	
+       } catch(IOException e) {
+    	   message = "ioexception " + e; 
+       } catch(Exception e) {
+    	   message = "exception " + e; 
+       }
+	   
+	   
 	   String dbvalue = null;
 		try {
 			
 			Class.forName("com.mysql.jdbc.Driver");  
 			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/insecureapp","insecureapp","45EUlZOpL7");  // #7 Hardcoded password
 			
-			String sql = "select * from users where userid = '" + input1 + "'";
+			String sql = "select * from users where userid = '" + text + "'";
 			PreparedStatement pstmt = connection.prepareStatement( sql ); 
             ResultSet rs = pstmt.executeQuery(sql);  
             
@@ -42,7 +72,7 @@ public class HelloWorld {
             	System.out.println("<p>Name: " + rs.getString(3) + " " + rs.getString(2) + "<br>Address: " + rs.getString(4) + "<br>Phone no: " + rs.getString(5) + "</p>"); // #10 Stored XSS
             	dbvalue = "<p>Name: " + rs.getString(3) + " " + rs.getString(2) + "<br>Address: " + rs.getString(4) + "<br>Phone no: " + rs.getString(5) + "</p>";
             }
-    		message = "updated message: " + input1 + " : " + dbvalue + ";";
+    		message = "updated message: " + text + " : " + dbvalue + ";";
             connection.close();
         } 
 		catch(Exception e){ 
